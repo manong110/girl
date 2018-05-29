7957a7e88a4c87d9af0159609fff28f2fdd73412
manong110 <m15617927390_1@163.com>
2018-05-29 15:24:23 +0800

    添加参数注解@Param

--- src/main/java/com/inspur/cn/mapper/DemoMapper.java
+++ src/main/java/com/inspur/cn/mapper/DemoMapper.java
@@ -3,6 +3,7 @@ package com.inspur.cn.mapper;
 import com.inspur.cn.repo.Demo;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -10,13 +11,13 @@ import java.util.List;
 public interface DemoMapper {
 
     @Select("select * from Demo Where id=#{id}")
-    public Demo getById(Integer id);
+    public Demo getById(@Param("id") Integer id);
 
     @Select("select name from Demo Where id=#{id}")
-    public String getNameById(Integer id);
+    public String getNameById(@Param("id") Integer id);
 
     @Select("select * from Demo Where name=#{name}")
-    public List<Demo> findName(String name);
+    public List<Demo> findName(@Param("name") String name);
 
     @Insert("insert into Demo(name) values(#{name})")
     @Options(useGeneratedKeys = true, keyColumn = "id",keyProperty = "id")
